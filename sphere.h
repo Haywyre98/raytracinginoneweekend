80b08f05104cafa734599b94f7973ae84bef072e
Haywyre98 <chris.kovacs@outlook.com>
2017-04-16 13:51:49 +0800

    Update sphere.h

--- sphere.h
+++ sphere.h
@@ -5,10 +5,11 @@ class sphere : public hitable
 {
 	public:
 		sphere() {}
-		sphere(vec3 cen, float r) : center(cen), radius(r) {};
+		sphere(vec3 cen, float r) : center(cen), radius(r), mat_ptr(m) {};
 		virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
 		vec3 center;
 		float radius;
+		material *mat_ptr;
 };
 
 bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const
